a7a2966adbf647697804b4326c5af27cab29ac3c
Arumulla Yaswanth Reddy <yaswanth.arumulla@gmail.com>
2026-08-15 20:13:41 +0530

    fix: Fluentd output config with ES credentials (user/password for xpack auth)

--- Episode-10/terraform/modules/logging/main.tf
+++ Episode-10/terraform/modules/logging/main.tf
@@ -249,6 +249,50 @@ resource "harness_platform_gitops_applications" "fluentd" {
             name  = "kind"
             value = "DaemonSet"
           }
+          values = <<-EOT
+            fileConfigs:
+              01_sources.conf: |-
+                <source>
+                  @type tail
+                  @id in_tail_container_logs
+                  path /var/log/containers/*.log
+                  pos_file /var/log/fluentd-containers.log.pos
+                  tag kubernetes.*
+                  read_from_head true
+                  <parse>
+                    @type cri
+                  </parse>
+                </source>
+              02_filters.conf: |-
+                <filter kubernetes.**>
+                  @type kubernetes_metadata
+                  @id filter_kube_metadata
+                </filter>
+              04_outputs.conf: |-
+                <match **>
+                  @type elasticsearch
+                  @id out_es
+                  host elasticsearch-master
+                  port 9200
+                  user elastic
+                  password ${random_password.efk.result}
+                  scheme http
+                  logstash_format true
+                  logstash_prefix fluentd
+                  include_tag_key true
+                  <buffer>
+                    @type file
+                    path /var/log/fluentd-buffers/kubernetes.system.buffer
+                    flush_mode interval
+                    flush_interval 5s
+                    retry_type exponential_backoff
+                    retry_max_interval 30
+                    chunk_limit_size 2M
+                    queue_limit_length 8
+                    overflow_action block
+                  </buffer>
+                </match>
+          EOT
         }
       }
       destination {
