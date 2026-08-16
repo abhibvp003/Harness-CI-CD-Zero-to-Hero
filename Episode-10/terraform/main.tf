784188f1e36dfc60f4af06238dfb6f8c3fbf82c3
Arumulla Yaswanth Reddy <yaswanth.arumulla@gmail.com>
2026-08-16 22:14:20 +0530

    fix: StorageClass provisioner ebs.csi.aws.com (not eks.amazonaws.com which is Auto Mode only)

--- Episode-10/terraform/main.tf
+++ Episode-10/terraform/main.tf
@@ -46,7 +46,7 @@ resource "kubernetes_storage_class" "ebs" {
     name        = "auto-ebs-sc"
     annotations = { "storageclass.kubernetes.io/is-default-class" = "true" }
   }
-  storage_provisioner    = "ebs.csi.eks.amazonaws.com"
+  storage_provisioner    = "ebs.csi.aws.com"
   volume_binding_mode    = "WaitForFirstConsumer"
   allow_volume_expansion = true
   parameters             = { type = "gp3", encrypted = "true" }
