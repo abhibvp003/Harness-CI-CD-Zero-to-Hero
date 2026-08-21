8e039d6c73ca48f781b0b339739ce677a5737e7a
Arumulla Yaswanth Reddy <yaswanth.arumulla@gmail.com>
2026-08-21 13:26:07 +0530

    fix: add discovery.type single-node to ES config (single node goes GREEN, readiness probe passes)

--- Episode-10/terraform/modules/logging/main.tf
+++ Episode-10/terraform/modules/logging/main.tf
@@ -125,7 +125,7 @@ resource "harness_platform_gitops_applications" "elasticsearch" {
           }
           parameters {
             name  = "esConfig.elasticsearch\\.yml"
-            value = "xpack.security.enabled: true\nxpack.license.self_generated.type: trial\nxpack.security.transport.ssl.enabled: false\n"
+            value = "xpack.security.enabled: true\nxpack.license.self_generated.type: trial\nxpack.security.transport.ssl.enabled: false\ndiscovery.type: single-node\n"
           }
         }
       }
